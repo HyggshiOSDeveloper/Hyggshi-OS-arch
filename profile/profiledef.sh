dac950052634f6517aed16e6267e27c1690acbb9
Hyggshi OS <tfhy5321@gmail.com>
2026-09-23 20:02:33 +0700

    Create profiledef.sh

---
+++ profile/profiledef.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="hyggshi-os-arch"
+iso_label="HYGGSHI_$(date +%Y%m)"
+iso_publisher="Hyggshi OS <https://github.com/Hyggshi-OS-project-center>"
+iso_application="Hyggshi OS Arch Live/Rescue DVD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/customize_airootfs.sh"]="0:0:755"
+)
